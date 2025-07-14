7a5aef944b2d48269dad71f05f4f16581b76487f
Fazrul <mfazrul07@gmail.com>
2025-07-14 17:17:32 +0800

    update the scss

--- src/app/shared/components/card-profile-intro/card-profile-intro.component.ts
+++ src/app/shared/components/card-profile-intro/card-profile-intro.component.ts
@@ -9,7 +9,6 @@ import {MatCard} from '@angular/material/card';
 import {MatChip, MatChipListbox} from '@angular/material/chips';
 import {COMMON_CONSTANTS} from '../../constants/common.constants';
 import {MatIcon} from '@angular/material/icon';
-import {CardLeetcodeComponent} from '../card-leetcode/card-leetcode.component';
 
 @Component({
   selector: 'app-card-profile-intro',
@@ -21,8 +20,7 @@ import {CardLeetcodeComponent} from '../card-leetcode/card-leetcode.component';
     MatCard,
     MatChip,
     MatChipListbox,
-    MatIcon,
-    CardLeetcodeComponent
+    MatIcon
   ],
   templateUrl: './card-profile-intro.component.html',
   styleUrl: './card-profile-intro.component.scss'
